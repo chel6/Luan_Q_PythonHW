df0987577e43a712c8bc8ccf0b76ee01db701fdd
Luan <q_luan@ats-hr420-dm05.fc.ca>
2019-10-29 14:23:48 -0400

    pull request problem

--- README.md
+++ README.md
@@ -1,11 +1,18 @@
-# Rock Paper Scissors in Python!
+# ROCK Paper Scissors in Python!
+
+Pick your weapon and play against the computer - you'll only get a virtual sting if you lose!
+
+Prerequisites: All you need is Python3 to play.
+
+Clon the project, navigate to the correct directory, - cd ditctory name - and run the game.py file in a Python Terminal window for macOS/Linux.
+
+The rules are the same as the classic game:
 1. Paper covers Rock
 2. Scissors cuts Paper
 3. Rock smashes Scissors
-Built with Python3
+
 Choose your weapon wisely!
-Clon the project, navigate to the correct directory, - cd ditctory name - and run the game.py file in a Python Terminal window for macOS/Linux.
-Pick your weapon and play against the computer - you'll only get a virtual sting if you lose!
-Prerequisites: All you need is Python3 to play.
+
 The computer chooses first; just follow the prompts.
-The rules are the same as the classic game:
+
+Built with Python3.
--- game.py
+++ game.py
@@ -1,6 +1,10 @@
 # import the random package so that we can generate a random choice
 from random import randint
 
+#to defined the lives
+player_lives = 5
+computer_lives = 5
+
 # choices is an array => an array is a container that can hold multiple values
 choices = ["rock", "paper", "scissors"]
 
@@ -14,10 +18,13 @@ while player is False:
 	# set player to True
 	
 	print("************************************\n\n")
+	print("Computer lives:", computer_lives,"/5","\n")
+	print("Player lives:", player_lives,"/5","\n")
 	print("Choose your weapon!\n\n")
 	print("************************************\n\n")
 	
 	player = input("choose rock, paper or scissors\n")
+	player = player.lower()
 
 	print("computer chose", computer, "\n")
 	print("player chose", player, "\n")
@@ -30,8 +37,10 @@ while player is False:
 	elif player.lower() == "rock":
 		if computer == "paper":
 			print("You Lose!", computer, "covers", player, "\n")
+			player_lives = player_lives - 1
 		else:
 			print("You Win!", player, "smashes", computer, "\n")
+			computer_lives = computer_lives - 1
 
 	elif player.lower() == "paper":
 		if computer == "scissors":
@@ -48,6 +57,36 @@ while player is False:
 	else:
 		print("That's not a valid choice, try again")
 
+	# handle all lives lost for player or AI
+	if player_lives is 0:
+		print("Out of lives! You suck at this game. Would you like to play again?\n")
+		choice = input("Y / N")
+		print(choice)
+
+		if (choice is "N") or(choice is "n"):
+			print("You chose to quit.")
+			exit()
+		elif (choice is "Y") or (choice is "y"):
+			#reset the game so that we can start all over again
+			player_lives = 5
+			computer_lives = 5
+			player = False
+			computer = choices[randint(0,2)]
+
+	if computer_lives is 0:
+		print("Cumputer is out of lives! You rock at this game. Would you like to play again?\n")
+		choice = input("Y / N")
+		print(choice)
+
+		if (choice is "N") or(choice is "n"):
+			print("You chose to quit.")
+			exit()
+		elif (choice is "Y") or (choice is "y"):
+			#reset the game so that we can start all over again
+			player_lives = 5
+			computer_lives = 5
+			player = False
+			computer = choices[randint(0,2)]
 
 	# need to check all of our conditions ager checking for a tie
 	player = False
