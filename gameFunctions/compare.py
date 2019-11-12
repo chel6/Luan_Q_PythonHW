8ef8d916f191e040226f42c9635c6ed2c3603919
Luan <q_luan@ATS-HR114-DM02.fc.ca>
2019-11-12 18:38:37 -0500

    add compare

--- gameFunctions/compare.py
+++ gameFunctions/compare.py
@@ -1,34 +1,40 @@
-from gameFunctions import gameVars
-
+from random import randint
+from gameFunctions import compare, winlose, gameVars
 # what are you trying to compare inside this function?
 # you will need to pass those things in as arguments
 # inside the round brackets
-def comparechoices():
+def comparechoices(computer, player):
 
 	if gameVars.player == "quit":
-			exit()
-		elif gameVars.computer == gameVars.player:
-			print("tie! no one wins, play again")
-	
-		elif gameVars.player.lower() == "rock":
+		exit()
+
+	elif gameVars.computer == gameVars.player:
+		print("tie! no one wins, play again~ (ง •̀_•́)ง ")
+	elif gameVars.player != gameVars.computer:
+
+		if gameVars.player == "rock":
 			if gameVars.computer == "paper":
-				print("You Lose!", gameVars.computer, "covers", gameVars.player, "\n")
+				print("You Lose! ...(｡•ˇ‸ˇ•｡) ... ", gameVars.computer, "covers", gameVars.player, "\n")
 				gameVars.player_lives = gameVars.player_lives - 1
-			else:q
-				print("You Win!", gameVars.player, "smashes", gameVars.computer, "\n")
+			else:
+				print("You Win! (੭≧▽≦)੭ ", gameVars.player, "smashes", gameVars.computer, "\n")
 				gameVars.computer_lives = gameVars.computer_lives - 1
 
-		elif gameVars.player.lower() == "paper":
+		elif gameVars.player == "paper":
 			if gameVars.computer == "scissors":
-				print("You Lose!", gameVars.computer, "cuts", gameVars.player, "\n")
+				print("You Lose! ...(｡•ˇ‸ˇ•｡) ...", gameVars.computer, "cuts", gameVars.player, "\n")
 			else:
-				print("You Win!", gameVars.player, "smashes", gameVars.computer, "\n")
+				print("You Win! (੭≧▽≦)੭ ", gameVars.player, "smashes", gameVars.computer, "\n")
 
-		elif gameVars.player.lower() == "scissors":
+		elif gameVars.player == "scissors":
 			if gameVars.computer == "rock":
-				print("You Lose!", gameVars.computer, "smashes", gameVars.player, "\n")
+				print("You Lose! ...(｡•ˇ‸ˇ•｡) ... ", gameVars.computer, "smashes", gameVars.player, "\n")
 			else:
-				print("You Win!", gameVars.player, "cuts", gameVars.computer, "\n")
+				print("You Win! (੭≧▽≦)੭ ", gameVars.player, "cuts", gameVars.computer, "\n")
 
-		else:
-			print("That's not a valid choice, try again")
+	else:
+		print("That's not a valid choice, try again (๑>◡<๑) ")
+
+
+	gameVars.player = False
+	gameVars.computer = gameVars.choices[randint(0,2)]
--- gameFunctions/winlose.py
+++ gameFunctions/winlose.py
@@ -25,9 +25,9 @@ def winorlose(status):
 		# global choices
 
 		# this will break, currently - we will fix this next class
-		gameVars.player_lives = 1
-		gameVars.computer_lives = 1
-		gameVars.total_lives = 1
+		gameVars.player_lives = 3
+		gameVars.computer_lives = 3
+		gameVars.total_lives = 3
 		gameVars.player = False
 		gameVars.computer = gameVars.choices[randint(0,2)]
 	
